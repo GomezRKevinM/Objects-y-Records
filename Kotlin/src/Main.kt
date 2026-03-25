e865bf5781824d859a141229e07f2e74c27c9df9
kevin-desktop <kgomezr2@unicartagena.edu.co>
2026-03-24 19:58:18 -0500

    feat: 2.Inicialización, creando 3 insstancias y añadiendolas a un array - Kevin Gómez

--- Kotlin/src/Main.kt
+++ Kotlin/src/Main.kt
@@ -1,29 +1,9 @@
-import Records.Estudiante
+import Objects.Estudiante
 
 fun main() {
-    val estudiante = Estudiante("Kevin",24,4.4);
-    val estudiante2 = Estudiante("Andres",31,4.8);
-    val estudiante3 = Estudiante("Maria",22,5.0);
+    val estudiante = Estudiante("Kevin",24,3.5)
+    val estudiante2 = Estudiante("Andres", 44, 3.0)
+    val estudiante3 = Estudiante("Jimena",22,3.8)
 
-    val estudiantes = listOf(estudiante, estudiante2, estudiante3);
-
-    estudiantes.forEach{ (nombre, edad, promedio) ->
-        println("Información del Estudiante")   
-        println("\tNombre del estudiante: $nombre")
-        println("\tEdad del estudiante: $edad")
-        println("\tPromedio del estudiante: $promedio\n")
-    }
-
-
-    estudiante2.promedio = 2.5;
-        println("Información del Estudiante")   
-        println("\tNombre del estudiante: ${estudiante2.nombre}")
-        println("\tEdad del estudiante: ${estudiante3.edad}")
-        println("\tPromedio del estudiante: ${estudiante2.promedio}\n")
-
-    for(estudent in estudiantes){
-        estudent.promedio = 5.0;
-    }
-
-    println("Estudiantes: ${estudiantes}")
+    val estudiantes = listOf(estudiante, estudiante2, estudiante3)
 }
